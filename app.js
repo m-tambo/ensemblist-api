cb70a7258d70e7053c2c2f89ca3bbd8349453466
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-05 00:11:35 -0500

    minor adjustments

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const { json } = require('body-parser')
 
-const routes = require('routes') // automatially looks for index.js file
+const routes = require('./routes') // automatically looks for index.js file
 
 app.use(json()) // converts any req.body to json, via body-parser
 
 app.use('/api/v1', routes)
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
-  console.log(`Listening on port ${port} in this super keen env: ${process.env.NODE_ENV}`);
+  console.log(`Listening on port ${port} in env: ${process.env.NODE_ENV}`);
 });
 
 module.exports = app;
